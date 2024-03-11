60c02bfdfd0d12f2f45fad320cd8d2e7e51372fc
Vo-A-Qui <vohqui51202@icloud.com>
2024-03-11 14:21:04 +0700

    ok

--- routes/lab1_05.js
+++ routes/lab1_05.js
@@ -1,5 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const users = [
+    { id: 1, name: 'Trần thị hường', email: 'huongtt@fpt.edu.vn', phone: '0908732558', pass: '123456' },
+    { id: 2, name: 'Võ A Qui', email: 'quit@fpt.edu.vn', phone: '01203654463', pass: '123' },
+];
+
+// Middleware để phân tích cú pháp JSON trong phần thân yêu cầu
+router.use(express.json());
+
 router.get('/bai1', function (req, res, next) {
     res.json({ title: 'AQui' });
 });
@@ -11,18 +19,46 @@ router.get('/bai2', function (req, res, next) {
 });
 
 router.get('/bai3', function (req, res, next) {
-    const { a , b } = req.query;
-    const name = 'huongtt'
-    const pass = '123'
-    if (a == name &&  b == pass){
-        res.json({ kq: 'đăng nhập thành công'});
-    }else{
-        res.json({ kq: 'đăng nhập thất bại'});
+    const { a, b } = req.query;
+    const name = 'huongtt';
+    const pass = '123';
+    if (a == name && b == pass) {
+        res.json({ kq: 'đăng nhập thành công' });
+    } else {
+        res.json({ kq: 'đăng nhập thất bại' });
     }
 });
 
+router.post('/register', function (req, res, next) {
+    const { name, email, phone, pass } = req.body;
 
+    // Kiểm tra xem tất cả các trường bắt buộc có được cung cấp không
+    if (name && email && phone && pass) {
+        const newUser = {
+            id: users.length + 1,
+            name: name,
+            email: email,
+            phone: phone,
+            pass: pass,
+        };
 
+        // Thêm người dùng mới vào mảng hiện tại
+        users.push(newUser);
 
+        res.json({ kq: 'Đăng ký thành công' });
+    } else {
+        res.json({ kq: 'Đăng ký thất bại, vui lòng nhập đầy đủ thông tin' });
+    }
+});
+
+router.get('/bai4', function (req, res, next) {
+    const { a, b } = req.query;
+    const user = users.find(user => user.email === a && user.phone === b);
+    if (user) {
+        res.json({ kq: 'Đăng nhập thành công' });
+    } else {
+        res.json({ kq: 'Đăng nhập thất bại' });
+    }
+});
 
 module.exports = router;
